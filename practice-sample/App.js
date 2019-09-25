8edd4aba2766c64139ffe60a9245146f1924166c
yusukeoyamada <fa19940118@gmail.com>
2019-09-25 17:32:01 +0900

    [practice-sample] サンプルアプリ実装完了

--- practice-sample/App.js
+++ practice-sample/App.js
@@ -1,19 +1,91 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  FlatList,
+  Dimensions,
+  ActivityIndicator
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      threads: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://www.reddit.com/r/newsokur/hot.json")
+      .then(response => response.json())
+      .then(responseJson => {
+        let threads = responseJson.data.children;
+        threads = threads.map(i => {
+          i.key = i.data.url;
+          return i;
+        });
+        this.setState({ threads: threads, isLoading: false });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    const { threads, isLoading } = this.state;
+    const { width } = Dimensions.get("window");
+
+    return (
+      <View style={styles.container}>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <FlatList
+            data={threads}
+            renderItem={({ item }) => {
+              return (
+                <View style={styles.flatlist}>
+                  <Image
+                    style={styles.image}
+                    source={{
+                      uri: item.data.thumbnail
+                    }}
+                  />
+                  <View style={{ width: width - 50 }}>
+                    <View style={{ flex: 1, flexDirection: "column" }}>
+                      <Text>{item.data.title}</Text>
+                      <Text style={{ color: "#000080", fontSize: 10 }}>
+                        {item.data.domain}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              );
+            }}
+          />
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 46
   },
+  flatlist: {
+    flex: 1,
+    flexDirection: "row",
+    width: "100%"
+  },
+  image: {
+    width: 50,
+    height: 50
+  }
 });
